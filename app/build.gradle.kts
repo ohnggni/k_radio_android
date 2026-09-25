ce57bd4bcb8793baa14beae7261998d4aced0d30
ohnggni <ohnggni@naver.com>
2026-09-25 09:48:14 +0900

    Release 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "kr.ohnggni.kradio"
         minSdk = 26
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
